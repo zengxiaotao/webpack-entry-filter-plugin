5265ceb73a810ae700a4c5c69c796d98e67a7deb
zengxiaotao <zengxiaotao@kuaishou.com>
2023-03-20 23:44:34 +0800

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import type { Compiler, EntryNormalized } from 'webpack';
+import fs from 'fs';
+import minimatch from 'minimatch';
+
+type PluginOptions = {
+
+}
+
+const localeFileName = `.entryrc`;
+
+export class WebpackEntryFilterPlugin {
+  options: PluginOptions;
+  includes!: string[];
+
+  constructor(options: PluginOptions) {
+    this.options = options;
+    this.includes = [];
+    this._loadLocaleFile();
+  }
+
+  _loadLocaleFile() {
+    try {
+      this.includes = fs.readFileSync(localeFileName, 'utf-8')
+          .split('\n')
+          .filter((line) => line && !line.startsWith('#'))
+      console.log('加载到本地 entry 配置', this.includes);
+    } catch (e) {
+      return;
+    }
+  }
+
+  apply(compiler: Compiler) {
+    compiler.hooks.entryOption.tap('WebpackEntryFilterPlugin', (_: any, entry: EntryNormalized) => {
+      // 不满足从 entry 中 delete
+      Object.keys(entry).forEach((key) => {
+        if (this.includes.length && !this.includes.some((include) => minimatch(key, include))) {
+          // @ts-ignore
+          delete entry[key];
+        }
+      })
+
+      return undefined as any;
+    })
+  }
+}
